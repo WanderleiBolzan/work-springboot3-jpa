a9fa19e20a46659e381bd3e0bb4efad5a63c9b86
wanderlei jose bolzan <wbolzanjp@gmail.com>
2024-04-07 13:10:28 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/loja/entities/User.java
+++ src/main/java/com/educandoweb/loja/entities/User.java
@@ -3,9 +3,21 @@ package com.educandoweb.loja.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="Tb_User")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
