34e6f227781f0105360644855f3dd1eb4b037f8b
wanderlei jose bolzan <wbolzanjp@gmail.com>
2024-04-07 19:45:48 -0300

    Service layer, component registration  - PARTE 2

---
+++ src/main/java/com/educandoweb/loja/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.loja.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.loja.entities.Order;
+import com.educandoweb.loja.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
